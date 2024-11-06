ff80e5c87b30bc20b90d64dabb381e79c688cf05
Milan <itsnotmily@gmail.com>
2024-11-06 22:37:00 +0000

    Update js.js

--- js/js.js
+++ js/js.js
@@ -271,34 +271,6 @@ function test() {
 	xmlhttp.send();
 }
 
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-	scrollFunction();
-};
-
-/*
-function scrollFunction() {
-	if (
-		document.body.scrollTop > 20 ||
-		document.documentElement.scrollTop > 20
-	) {
-		mybutton.style.display = "block";
-	} else {
-		mybutton.style.display = "none";
-	}
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
-}
-*/
-
 function storieschange() {
 	if ($('#collstories').hasClass('show') == false && document.querySelectorAll("#colltester")[0].innerHTML.split("(Click a story title to read more)").length<2){
 		document.querySelectorAll("#colltester")[0].innerHTML = document.querySelectorAll("#colltester")[0].innerHTML.replace("Stories", "Stories (Click a story title to read more)");
