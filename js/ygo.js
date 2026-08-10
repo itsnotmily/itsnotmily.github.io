71f8c0862906eecc5f89e380fba825e897bbc3ee
Milan <itsnotmily@gmail.com>
2026-08-10 22:24:45 +0100

    Add Shaddoll card IDs and tidy HTML
    Expand ygocards in js/ygo.js with many Shaddoll-related card IDs (Void Unleashing, Lev Shaddoll Fusion, El Shaddoll Schism, Tohushaddoll Grysta, El Shaddoll Meshachrer, El Shaddoll Fusion, El Shaddoll Construct, Shaddoll Hedgehog, Reeshaddoll Wendi, El Shaddoll Wendikurhu, Naelshaddoll Ariel, El Shaddoll Shekhinaga) and add placeholder entries for El Shaddoll Winda and Elder Entity N'tss. Also insert an <!-- endAlternate path --> marker and remove extra blank-line spacing in gaming/yugioh/shaddoll/index.html.

--- js/ygo.js
+++ js/ygo.js
@@ -1,6 +1,20 @@
 const ygocards = {
   "Shaddoll Apkallone": "50907446",
-  "Shaddoll Squamata": "30328508"
+  "Shaddoll Squamata": "30328508",
+  "Void Unleashing": "61345801",
+  "Lev Shaddoll Fusion": "34950192",
+  "El Shaddoll Schism": "21011044",
+  "Tohushaddoll Grysta": "95072744",
+  "El Shaddoll Meshachrer": "32467459",
+  "El Shaddoll Fusion": "6417578",
+  "El Shaddoll Construct": "20366274",
+  "Shaddoll Hedgehog": "4939890",
+  "Reeshaddoll Wendi": "51023024",
+  "El Shaddoll Wendikurhu": "8852158",
+  "Naelshaddoll Ariel": "97518132",
+  "El Shaddoll Shekhinaga": "74822425",
+  "El Shaddoll Winda": "000000",
+  "Elder Entity N'tss": "80532587"
 };
 
 document.querySelectorAll("card-link").forEach(el => {
